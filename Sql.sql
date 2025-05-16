ae7748e088ccbaf5c85d1c013e4f0d4431ce49b9
Duarte Escarmeia <duarteescarameia2435a@gmail.com>
2025-05-16 23:00:35 +0100

    Update Sql.sql

--- Sql.sql
+++ Sql.sql
@@ -1,1 +1,9 @@
-SELECT * FROM tabela;
+-- Tabela de exemplo para o QR School
+CREATE TABLE Reservas (
+    ID INTEGER PRIMARY KEY,
+    AlunoID INTEGER,
+    PortatilID INTEGER,
+    Data DATE,
+    HoraInicio TIME,
+    HoraFim TIME
+);
